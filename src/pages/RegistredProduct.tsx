c88b02c8f7fa1b6f5ade22cbcd6b3dfa1c8bf5fe
Gabriel372 <gabriel-dobt@hotmail.com>
2024-03-17 16:18:22 -0300

    update

--- src/pages/RegistredProduct.tsx
+++ src/pages/RegistredProduct.tsx
@@ -13,12 +13,13 @@ const {  BoxProduct,setBoxProduct } = useContext(CatalogContext) as TBoxProduct;
 return <div>
 <div className='sm:w-auto mx-auto sm:max-w-[1100px] px-2 flex  flex-col min-h-[calc(95vh)] '>
 <h3 className='text-2xl text-center my-2'>Produtos Cadastrados</h3>
-
+<p className=' screen400:bg-red-500 screen450:bg-blue-500 bg-pink-500 text-center '>TESTE</p>
 <div>
+{/* <ul className='custom:flex-col custom:items-center custom:flex  sm:grid sm:gap-3 sm:grid-cols-2 md:grid-cols-3 '>{BoxProduct.map((product)=>( */}
+{/* <ul className='flex screen1:flex-col screen1:items-center flex-col items-center screen2:grid screen2:gap-3 screen2:grid-cols-2'>{BoxProduct.map((product)=>( */}
 
 {BoxProduct.length > 0 ?
-<ul className='flex flex-col items-center sm:grid sm:gap-3 sm:grid-cols-2 md:grid-cols-3   '
->{BoxProduct.map((product)=>(
+<ul className='border-spacing-2 screen1:border-red-600 sreen2:border-blue-500 ' >{BoxProduct.map((product)=>(
 <RegistredCardProduct product={product}/>
 ))}
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      'screen550': {'max': '550px'},
+      'screen500': {'max': '500px'},
+      'screen450': {'max': '450px'},
+      'screen400': {'max': '400px'},
+
+      // 'screen2': {'max': '700px'},
+      // 'screen1': {'max': '500px'},
+    },
+
   },
   plugins: [],
 }
